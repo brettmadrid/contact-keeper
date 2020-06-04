4c00c196ce92d52d9b72ed869126ebe03ba3945d
brettmadrid <brettmadrid9@gmail.com>
2020-06-03 21:44:11 -0700

    setup users CRUD function shells

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
+
+// Connect Database
+connectDB()
+
+// Init Middleware
+app.use(express.json({ extended: false })) // allows app to understand json data
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the ContactKeeper API...' })
